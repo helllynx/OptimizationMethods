c5a4bff2d71089672720cf339b469e3bdbfe2d10
noname <fuckyou@notyourbisness.com>
2018-09-08 03:23:51 +0300

    OMG 6-th ex DONE

--- Lab1/optimize.py
+++ Lab1/optimize.py
@@ -50,6 +50,22 @@ def bisection(fun_prime, a, b, e):
     return c, i
 
 
+def bisection_numeric(fun, diff, a, b, e):
+    i = 0
+    c = (a + b) / 2
+    while (b - a) / 2 > e:
+        if diff(fun, e, c) == 0:
+            i += 1
+            return c, i
+        elif diff(fun, e, a) * diff(fun, e, c) < 0:
+            i += 1
+            b = c
+        else:
+            a = c
+        c = (a + b) / 2
+    return c, i
+
+
 def gss(fun, a, b, e=0.001):
     i = 0
     gr = (np.sqrt(5) + 1) / 2  # equals 1.618....
@@ -121,6 +137,19 @@ def middle_point(fun_prime, a, b, e, n):
             else:
                 a = x
 
+def middle_point_numeric(fun, diff, a, b, e, n):
+    i = 0
+    for _ in range(0, n):
+        i += 1
+        x = (a + b) / 2
+        f_p = diff(fun, e, x)
+        if np.abs(f_p) < e:
+            return x, i
+        else:
+            if f_p > 0:
+                b = x
+            else:
+                a = x
 
 def chords(fun_prime, a, b, e, n):
     i = 0
@@ -136,6 +165,20 @@ def chords(fun_prime, a, b, e, n):
             else:
                 a = X
 
+
+def chords_numeric(fun, diff, a, b, e, n):
+    i = 0
+    for _ in range(0, n):
+        i += 1
+        X = a - (diff(fun, e, a) / (diff(fun, e, a) - diff(fun, e, b))) * (a - b)
+        f_p = diff(fun, e, X)
+        if np.abs(f_p) < e:
+            return X, i
+        else:
+            if f_p > 0:
+                b = X
+            else:
+                a = X
 
 def plot(x, y, label_x="", label_y="", title=""):
     plt.plot(x, y)
@@ -174,6 +217,18 @@ def newton(fun_prime, fun_second, x0, e, n):
             x = x - df / ddf
             i += 1
 
+def newton_numeric_diff(fun, diff, diff2, x0, e, n):
+    x = x0
+    i = 0
+    for _ in range(0, n):
+        df = diff(fun, e, x)
+        ddf = diff2(fun, e, x)
+        if np.abs(df) < e:
+            i += 1
+            return x, i
+        else:
+            x = x - df / ddf
+            i += 1
 
 def newton_rafson(fun_prime, fun_second, x0, e, n):
     x = x0
@@ -191,6 +246,7 @@ def newton_rafson(fun_prime, fun_second, x0, e, n):
             x = x - t * df / ddf
             i += 1
 
+
 def newton_rafson_numeric(fun, x0, e, n):
     x = x0
     i = 0
@@ -207,9 +263,12 @@ def newton_rafson_numeric(fun, x0, e, n):
             x = x - t * df / ddf
             i += 1
 
-def newton(fun_prime, fun_second, x0, e, n):
+
+def newton_markvardt(fun, fun_prime, fun_second, x0, e, n):
     x = x0
     i = 0
+    f = fun(x)
+    mu = fun_second(x)
     for _ in range(0, n):
         df = fun_prime(x)
         ddf = fun_second(x)
@@ -217,8 +276,35 @@ def newton(fun_prime, fun_second, x0, e, n):
             i += 1
             return x, i
         else:
-            x = x - df / ddf
+            x = x - df / (ddf + mu)
+            nf = fun(x)
             i += 1
+        if nf < f:
+            mu = mu / 2
+        else:
+            mu = mu * 2
+
+
+def newton_markvardt_numeric(fun, x0, e, n):
+    x = x0
+    i = 0
+    f = fun(x)
+    mu = derivative(fun, x, n=2)
+    for _ in range(0, n):
+        df = derivative(fun, x, n=1)
+        ddf = derivative(fun, x, n=2)
+        if np.abs(df) < e:
+            i += 1
+            return x, i
+        else:
+            x = x - df / (ddf + mu)
+            nf = fun(x)
+            i += 1
+        if nf < f:
+            mu = mu / 2
+        else:
+            mu = mu * 2
+
 
 def massive_test(fun, fun_prime, fun_second, a, b, n, x0, e_start, e_end, e_step):
     test = {}
@@ -233,7 +319,13 @@ def massive_test(fun, fun_prime, fun_second, a, b, n, x0, e_start, e_end, e_step
     return test
 
 
-
+def massive_test_diff(fun, diff, diff2, a, b, n, x0, e_start, e_end, e_step):
+    test = {}
+    test["bisection"] = [[bisection_numeric(fun, diff, a, b, e)[1], e] for e in np.arange(e_start, e_end, -e_step)]
+    test["middle_point"] = [[middle_point_numeric(fun, diff, a, b, e, n)[1], e] for e in np.arange(e_start, e_end, -e_step)]
+    test["chords"] = [[chords_numeric(fun, diff, a, b, e, n)[1], e] for e in np.arange(e_start, e_end, -e_step)]
+    test["newton"] = [[newton_numeric_diff(fun, diff, diff2, x0, e, n)[1], e] for e in np.arange(e_start, e_end, -e_step)]
+    return test
 
 def unpack(data):
     return [d[1] for d in data], [d[0] for d in data]
@@ -259,6 +351,19 @@ def plot_test(data):
     plt.show()
 
 
+def plot_test_diff(data):
+    plt.style.use('ggplot')
+    x, y = unpack(data.get("bisection")[:])
+    plt.plot(x, y, label="bisection")
+    x, y = unpack(data.get("middle_point")[:])
+    plt.plot(x, y, label="middle_point")
+    x, y = unpack(data.get("chords")[:])
+    plt.plot(x, y, label="chords")
+    x, y = unpack(data.get("newton")[:])
+    plt.plot(x, y, label="newton")
+    plt.legend(bbox_to_anchor=(1, 1), loc=1, borderaxespad=0.)
+    plt.show()
+
 def myfunc(x):
     return pow(x, 4) + pow(x, 2) + x + 1
 
@@ -269,6 +374,22 @@ def myfunc_prime(x):
 
 def myfunc_second(x):
     return 12 * pow(x, 2) + 2
+
+
+def center(fun, h, x):
+    return (fun(x + h) - fun(x - h)) / (2 * h)
+
+
+def center_second(fun, h, x):
+    return (fun(x + h) - 2 * fun(x) + fun(x - h)) / (h ** 2)
+
+
+def left(fun, h, x):
+    return (fun(x) - fun(x - h)) / h
+
+
+def right(fun, h, x):
+    return (fun(x + h) - fun(x)) / h
 
 
 def newton_test_diff_type(fun, fun_prime, fun_second, a, b, e, n):
@@ -289,26 +410,11 @@ def newton_test_rafson_diff_type(fun, fun_prime, fun_second, a, b, e, n):
     return [newton_num, newton_analit]
 
 
-fun5 = lambda x: x * np.arctan(x) - 1 / 2 * np.log(1 + pow(x, 2))
-d_fun5 = lambda x: np.arctan(x)
-dd_fun5 = lambda x: 1 / (pow(x, 2) + 1)
-
-# x = np.arange(-1, 1, 0.001)
-# y = fun5(x)
-#
-# plot(x, y)
-#
-x, y = zip(*sorted((newton_test_diff_type(fun5, d_fun5, dd_fun5, -1, 1, 0.0001, 100)[1]).items()))
-plt.plot(x, y)
-x, y = zip(*sorted((newton_test_diff_type(fun5, d_fun5, dd_fun5, -1, 1, 0.0001, 100)[0]).items()))
-plt.plot(x, y)
-# plt.show()
-
-
-
-x, y = zip(*sorted((newton_test_rafson_diff_type(fun5, d_fun5, dd_fun5, -1, 1, 0.0001, 100)[1]).items()))
-plt.plot(x, y)
-x, y = zip(*sorted((newton_test_rafson_diff_type(fun5, d_fun5, dd_fun5, -1, 1, 0.0001, 100)[0]).items()))
-plt.plot(x, y)
-plt.show()
+def newton_test_markvardt_diff_type(fun, fun_prime, fun_second, a, b, e, n):
+    newton_num = {}
+    newton_analit = {}
+    for x0 in np.arange(a, b, 0.01):
+        newton_num[x0] = newton_markvardt_numeric(fun, x0, e, n)[1]
+        newton_analit[x0] = newton_markvardt(fun, fun_prime, fun_second, x0, e, n)[1]
+    return [newton_num, newton_analit]
 
